4f3c3b85bf9046fa7879af3bd84b90044f4849e9
강인화 <rkddlsghk1998@gmail.com>
2025-04-11 23:05:46 +0900

    블록킹 방식으로 변경

--- nonblocking_echo/nb_echo_server.c
+++ nonblocking_echo/nb_echo_server.c
@@ -5,8 +5,6 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/time.h>
-#include <sys/select.h>
-#include <fcntl.h>
 #include <errno.h>
 
 #define SERVERPORT 9000
@@ -19,110 +17,90 @@ void err_quit(const char *msg) {
 }
 
 int main() {
-    int server_sock, client_sock = -1;
+    int server_sock, client_sock;
     struct sockaddr_in server_addr, client_addr;
     char buf[BUFSIZE];
-    time_t last_recv_time;
+    int retval;
     
     // 소켓 생성
     server_sock = socket(AF_INET, SOCK_STREAM, 0);
     if (server_sock == -1) err_quit("socket()");
     
-    // SO_REUSEADDR 설정
+    // SO_REUSEADDR 소켓 옵션 설정
     int optval = 1;
-    if (setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1)
-        err_quit("setsockopt()");
+    retval = setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+    if (retval == -1) err_quit("setsockopt()");
     
     // bind
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(SERVERPORT);
-    if (bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
-        err_quit("bind()");
+    retval = bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (retval == -1) err_quit("bind()");
     
     // listen
-    if (listen(server_sock, SOMAXCONN) == -1)
-        err_quit("listen()");
+    retval = listen(server_sock, SOMAXCONN);
+    if (retval == -1) err_quit("listen()");
     
     printf("서버 시작...\n");
     
     while (1) {
-        fd_set readset;
-        struct timeval tv = {1, 0}; // 1초 타임아웃
-        
-        FD_ZERO(&readset);
-        FD_SET(server_sock, &readset);
-        
-        int maxfd = server_sock;
-        
-        // 클라이언트가 연결되어 있으면 해당 소켓도 감시
-        if (client_sock != -1) {
-            FD_SET(client_sock, &readset);
-            if (client_sock > maxfd)
-                maxfd = client_sock;
+        // accept()
+        socklen_t client_addr_size = sizeof(client_addr);
+        client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_addr_size);
+        if (client_sock == -1) {
+            err_quit("accept()");
+            continue;
         }
         
-        // select로 입출력 가능한 소켓 감시
-        int ready = select(maxfd + 1, &readset, NULL, NULL, &tv);
-        if (ready == -1) {
-            err_quit("select()");
-        }
+        // 클라이언트 접속 정보 출력
+        printf("\n[TCP 서버] 클라이언트 접속: IP=%s, Port=%d\n",
+               inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
         
-        // 새로운 클라이언트 연결 체크
-        if (FD_ISSET(server_sock, &readset)) {
-            if (client_sock == -1) {  // 현재 연결된 클라이언트가 없을 때만 새 연결 수락
-                socklen_t client_addr_size = sizeof(client_addr);
-                client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_addr_size);
-                
-                if (client_sock == -1) {
-                    err_quit("accept()");
-                }
-                
-                last_recv_time = time(NULL);
-                printf("\n[TCP 서버] 클라이언트 접속: IP=%s, Port=%d\n",
-                       inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-            }
-        }
+        // 수신 타임아웃 설정
+        struct timeval tv;
+        tv.tv_sec = TIMEOUT_SECONDS;
+        tv.tv_usec = 0;
+        retval = setsockopt(client_sock, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+        if (retval == -1) err_quit("setsockopt()");
         
-        // 연결된 클라이언트 처리
-        if (client_sock != -1 && FD_ISSET(client_sock, &readset)) {
-            // 타임아웃 체크
-            time_t current_time = time(NULL);
-            if (current_time - last_recv_time >= TIMEOUT_SECONDS) {
-                printf("클라이언트 타임아웃으로 연결 종료\n");
-                close(client_sock);
-                client_sock = -1;
-                continue;
-            }
-            
-            // 데이터 수신
+        // 클라이언트와 데이터 통신
+        while (1) {
+            // 데이터 받기
             memset(buf, 0, BUFSIZE);
-            int received = recv(client_sock, buf, BUFSIZE, 0);
+            retval = recv(client_sock, buf, BUFSIZE, 0);
+            if (retval == -1) {
+                if (errno == EWOULDBLOCK || errno == EAGAIN) {
+                    printf("클라이언트 타임아웃으로 연결 종료\n");
+                } else {
+                    printf("recv() 오류\n");
+                }
+                break;
+            } else if (retval == 0) {
+                printf("클라이언트 정상 종료\n");
+                break;
+            }
             
-            if (received <= 0) {
-                if (received == 0 || (errno != EAGAIN && errno != EWOULDBLOCK)) {
-                    printf("클라이언트 연결 종료\n");
-                    close(client_sock);
-                    client_sock = -1;
-                    continue;
-                }
-            } else {
-                // 데이터 수신 성공
-                last_recv_time = time(NULL);
-                printf("[TCP/%s:%d] %s\n", inet_ntoa(client_addr.sin_addr),
-                       ntohs(client_addr.sin_port), buf);
-                
-                // 에코
-                if (send(client_sock, buf, received, 0) == -1) {
-                    printf("send() 실패\n");
-                    close(client_sock);
-                    client_sock = -1;
-                }
+            // 받은 데이터 출력
+            printf("[TCP/%s:%d] %s\n", inet_ntoa(client_addr.sin_addr),
+                   ntohs(client_addr.sin_port), buf);
+            
+            // 데이터 보내기
+            retval = send(client_sock, buf, retval, 0);
+            if (retval == -1) {
+                printf("send() 오류\n");
+                break;
             }
         }
+        
+        // 소켓 닫기
+        close(client_sock);
+        printf("[TCP 서버] 클라이언트 종료: IP=%s, Port=%d\n",
+               inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
     }
     
+    // 서버 소켓 닫기
     close(server_sock);
     return 0;
 } 
