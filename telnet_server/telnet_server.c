b4f980b665fd89ddf4ee2294a2f669f3366b110f
강인화 <rkddlsghk1998@gmail.com>
2025-04-18 11:46:09 +0900

    feat: 텔넷 서버 수정

--- telnet_server/telnet_server.c
+++ telnet_server/telnet_server.c
@@ -5,88 +5,32 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/select.h>
-#include <fcntl.h>
 #include <errno.h>
 
-#define SERVERPORT 23  // 텔넷 기본 포트
+#define SERVERPORT 23
 #define BUFSIZE 512
-#define MAX_SESSIONS 10
-
-typedef struct {
-    int sock;
-    char username[32];
-    int authenticated;
-} Session;
+#define MAX_CLIENTS 10
 
 void err_quit(const char *msg) {
     perror(msg);
     exit(1);
 }
 
-void set_nonblocking_mode(int sock) {
-    int flags = fcntl(sock, F_GETFL, 0);
-    fcntl(sock, F_SETFL, flags | O_NONBLOCK);
-}
-
-void handle_telnet_command(Session *session, char *buf, int len) {
-    if (!session->authenticated) {
-        if (strncmp(buf, "login: ", 7) == 0) {
-            strncpy(session->username, buf + 7, sizeof(session->username) - 1);
-            session->username[strcspn(session->username, "\r\n")] = 0;
-            char *msg = "Password: ";
-            send(session->sock, msg, strlen(msg), 0);
-        }
-        else if (strncmp(buf, "password: ", 10) == 0) {
-            // 간단한 인증 (실제로는 더 안전한 방식 사용해야 함)
-            if (strncmp(buf + 10, "1234\r\n", 6) == 0) {
-                session->authenticated = 1;
-                char *msg = "\r\nWelcome to Telnet Server!\r\n$ ";
-                send(session->sock, msg, strlen(msg), 0);
-            }
-            else {
-                char *msg = "\r\nLogin failed!\r\nlogin: ";
-                send(session->sock, msg, strlen(msg), 0);
-            }
-        }
-        return;
-    }
-
-    // 명령어 처리
-    buf[strcspn(buf, "\r\n")] = 0;
-    if (strcmp(buf, "exit") == 0) {
-        char *msg = "Goodbye!\r\n";
-        send(session->sock, msg, strlen(msg), 0);
-        close(session->sock);
-        session->sock = -1;
-    }
-    else if (strcmp(buf, "help") == 0) {
-        char *msg = "Available commands:\r\n"
-                   "  help - show this help\r\n"
-                   "  exit - disconnect\r\n"
-                   "$ ";
-        send(session->sock, msg, strlen(msg), 0);
-    }
-    else {
-        char msg[BUFSIZE];
-        snprintf(msg, sizeof(msg), "Unknown command: %s\r\n$ ", buf);
-        send(session->sock, msg, strlen(msg), 0);
-    }
-}
-
 int main() {
     int server_sock;
     struct sockaddr_in server_addr;
-    Session sessions[MAX_SESSIONS] = {0};
+    int client_socks[MAX_CLIENTS] = {0};
     char buf[BUFSIZE];
     
+    // 소켓 생성
     server_sock = socket(AF_INET, SOCK_STREAM, 0);
     if (server_sock == -1) err_quit("socket()");
     
-    set_nonblocking_mode(server_sock);
-    
+    // SO_REUSEADDR 옵션 설정
     int optval = 1;
     setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
     
+    // bind
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -95,6 +39,7 @@ int main() {
     if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
         err_quit("bind()");
     
+    // listen
     if (listen(server_sock, SOMAXCONN) == -1)
         err_quit("listen()");
     
@@ -108,70 +53,73 @@ int main() {
         FD_SET(server_sock, &readfds);
         maxfd = server_sock;
         
-        for (int i = 0; i < MAX_SESSIONS; i++) {
-            if (sessions[i].sock > 0) {
-                FD_SET(sessions[i].sock, &readfds);
-                if (sessions[i].sock > maxfd)
-                    maxfd = sessions[i].sock;
+        // 클라이언트 소켓 설정
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (client_socks[i] > 0) {
+                FD_SET(client_socks[i], &readfds);
+                if (client_socks[i] > maxfd)
+                    maxfd = client_socks[i];
             }
         }
         
-        struct timeval tv = {0, 10000};
-        int ready = select(maxfd + 1, &readfds, NULL, NULL, &tv);
-        
-        if (ready == -1) {
+        // select() 호출
+        if (select(maxfd + 1, &readfds, NULL, NULL, NULL) == -1) {
             if (errno == EINTR) continue;
             err_quit("select()");
         }
         
+        // 새 클라이언트 연결 처리
         if (FD_ISSET(server_sock, &readfds)) {
             struct sockaddr_in client_addr;
             socklen_t client_addr_size = sizeof(client_addr);
             int client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_addr_size);
             
-            if (client_sock != -1) {
-                set_nonblocking_mode(client_sock);
-                
-                int i;
-                for (i = 0; i < MAX_SESSIONS; i++) {
-                    if (sessions[i].sock <= 0) {
-                        sessions[i].sock = client_sock;
-                        sessions[i].authenticated = 0;
-                        break;
-                    }
-                }
-                
-                if (i < MAX_SESSIONS) {
+            if (client_sock == -1) {
+                printf("accept() 오류\n");
+                continue;
+            }
+            
+            // 새 클라이언트 소켓 저장
+            int i;
+            for (i = 0; i < MAX_CLIENTS; i++) {
+                if (client_socks[i] == 0) {
+                    client_socks[i] = client_sock;
                     printf("새 클라이언트 연결: %d\n", client_sock);
-                    char *msg = "Welcome to Telnet Server\r\nlogin: ";
-                    send(client_sock, msg, strlen(msg), 0);
+                    break;
                 }
-                else {
-                    printf("최대 세션 수 초과\n");
-                    close(client_sock);
-                }
+            }
+            
+            if (i == MAX_CLIENTS) {
+                printf("최대 클라이언트 수 초과\n");
+                close(client_sock);
             }
         }
         
-        for (int i = 0; i < MAX_SESSIONS; i++) {
-            if (sessions[i].sock > 0 && FD_ISSET(sessions[i].sock, &readfds)) {
+        // 클라이언트 데이터 수신 처리
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (client_socks[i] > 0 && FD_ISSET(client_socks[i], &readfds)) {
                 memset(buf, 0, BUFSIZE);
-                int nbytes = recv(sessions[i].sock, buf, BUFSIZE - 1, 0);
+                int nbytes = recv(client_socks[i], buf, BUFSIZE - 1, 0);
                 
                 if (nbytes <= 0) {
-                    if (nbytes == 0 || errno != EWOULDBLOCK) {
-                        printf("클라이언트 종료: %d\n", sessions[i].sock);
-                        close(sessions[i].sock);
-                        sessions[i].sock = -1;
-                    }
+                    printf("클라이언트 종료: %d\n", client_socks[i]);
+                    close(client_socks[i]);
+                    client_socks[i] = 0;
                 }
                 else {
-                    handle_telnet_command(&sessions[i], buf, nbytes);
+                    // 받은 데이터를 모든 클라이언트에게 전송
+                    printf("[Client %d] %s", client_socks[i], buf);
+                    for (int j = 0; j < MAX_CLIENTS; j++) {
+                        if (client_socks[j] > 0) {
+                            send(client_socks[j], buf, nbytes, 0);
+                        }
+                    }
                 }
             }
         }
     }
     
+    // 서버 종료
     close(server_sock);
     return 0;
 } 
