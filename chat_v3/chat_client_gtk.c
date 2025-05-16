0d8f88cabbb1ba7b63691ee075bc2115a0f78ff1
강인화 <rkddlsghk1998@gmail.com>
2025-05-16 12:45:45 +0900

    fix: 클라이언트 버그 수정

--- chat_v3/chat_client_gtk.c
+++ chat_v3/chat_client_gtk.c
@@ -237,7 +237,7 @@ int main(int argc, char *argv[]) {
     GtkApplication *app;
     int status;
 
-    app = gtk_application_new("org.example.chatclient", G_APPLICATION_DEFAULT_FLAGS); // Use recommended flag
+    app = gtk_application_new("org.example.chatclient", G_APPLICATION_NEW_UNIQUE); // Use recommended flag
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
